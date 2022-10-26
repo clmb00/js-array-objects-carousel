8e63d010464be345c86588cd20d40abecd427646
clmb00 <paolo.colombo2000@gmail.com>
2022-10-26 14:58:59 +0200

    [add] basic layout

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const states = [
+  {
+    name: 'Sweden',
+    descr: 'Sweden, formally the Kingdom of Sweden, is a Nordic country in Scandinavia. The capital and largest city is Stockholm.',
+    image: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg'    
+  }, {
+    name: 'Peru',
+    descr: 'Peru, officially the Republic of Peru , is a country in western South America. Peru has a population of 32 million, and its capital and largest city is Lima.',
+    image: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg'    
+  }, {
+    name: 'Chile ',
+    descr: 'Chile, officially the Republic of Chile, is a country in the western part of South America. It is the southernmost country in the world, and the closest to Antarctica.',
+    image: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c'    
+  }, {
+    name: 'Argentina',
+    descr: 'Argentina, officially the Argentine Republic, is a country in the southern half of South America. Argentina covers an area of 2,780,400 km2',
+    image: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg'    
+  }, {
+    name: 'Colombia',
+    descr: 'Colombia, officially the Republic of Colombia, is a country in South America with an insular region in North America.',
+    image: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg'    
+  },
+]
